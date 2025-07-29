64d8770d1008f9ae9cae0873de4a527e7814d235
minho jang <mjang6988@gmail.com>
2025-07-29 12:00:30 +0900

    project init

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+const Content = () => {
+  return (
+    <div>
+      <h1>HTML to JSX 변환 연습</h1>
+      {/* JSX 로 변환해보세요 */}
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/SplitArticle.jsx
@@ -0,0 +1,19 @@
+const ArticleTitle = () => {
+  return;
+};
+
+const ArticleList = () => {
+  return (
+  );
+};
+
+const Article = () => {
+  return (
+    <article>
+      <ArticleTitle />
+      <ArticleList />
+    </article>
+  );
+};
+
+export default Article;
